821f2ad1ef043772229c64fe28dd6851d0591f48
julianchatto <juc226@lehigh.edu>
2024-09-20 19:03:13 -0400

    added a nav bar! I inculded buttons for each page, logo, and profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
-  return (
-    <>
-      <Dashboard></Dashboard>
-    </>
-  );
+  	return (
+		<>
+			{NavBar()}
+			<Dashboard></Dashboard>
+		</>
+  	);
 }
 
 export default App;
