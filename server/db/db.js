8d2cf911dcdc935fdcf01e4e6065d55d59194980
anjrajesh <anjrajesh16@gmail.com>
2024-09-20 23:03:39 -0400

    Fixed database connection issue

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,12 @@
-// db.js
+// intializing database connection
 import postgres from 'postgres'
+import dotenv from 'dotenv';
+dotenv.config();
 
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
-
+const connectionString = process.env.DATABASE_URL;
+const sql = postgres(connectionString);
+// const users = await sql`
+// select * from "dummyData"
+// `;
+// console.log(users);
 export default sql
