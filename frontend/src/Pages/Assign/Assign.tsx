ad0fa674a426873103ed49abed380ead706740a6
anjrajesh <anjrajesh16@gmail.com>
2024-09-21 07:41:55 -0400

    Added titles to the tables for clarity

--- frontend/src/Pages/Assign/Assign.tsx
+++ frontend/src/Pages/Assign/Assign.tsx
@@ -5,7 +5,10 @@ type Props = {}
 
 const Assign = (props: Props) => {
   return (
-    <div>{StickyHeadTable()}</div>
+    <>
+      <h1 className="title"><u>Waitlist of Clients</u></h1>
+      <div>{StickyHeadTable()}</div>
+    </>
   )
 }
 
--- frontend/src/Pages/Dashboard/Dashboard.tsx
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -6,10 +6,14 @@ type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <><div className="banner">
-      <h3><em>Lodestar is a light that guides the way.</em></h3>
-      <img src="https://cdn-icons-png.flaticon.com/512/3730/3730618.png" className="banner-image" alt="Lodestar icon" />
-    </div><div><StickyHeadTable /></div></>
+    <>
+      <div className="banner">
+        <h3><em>Lodestar is a light that guides the way.</em></h3>
+        <img src="https://cdn-icons-png.flaticon.com/512/3730/3730618.png" className="banner-image" alt="Lodestar icon" />
+      </div>
+      <h1 className="title"><u>Table of Current Clients</u></h1>
+      <div><StickyHeadTable /></div>
+    </>
   )
 }
 
