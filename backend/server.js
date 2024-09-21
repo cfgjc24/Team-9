1fa22d2c2cce0862df5360f7ec94c6cb8249edcf
Hayim Heron <hayimheron@gmail.com>
2024-09-21 00:24:06 -0400

    latest backend for dashboard

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,8 @@ app.get('/', (req, res) => {
 //const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON request bodies
+
+/*
 app.use(express.json());
 
 // Connect to MongoDB
@@ -96,31 +98,4 @@ app.post('/addresses', async (req, res) => {
     }
 });
 
-
-
-
-/*
-function createData(
-    id: string,
-    address: string,
-    last_appointment: string,
-    employee: string,
-    next_appointment: string
-): Dashboard {
-    return { id, address, last_appointment, employee, next_appointment };
-}
-
-
-const rows = [
-    createData(),
-    createData("2", "5678 Elm St", "2021-10-02", "Jane Doe", "2021-10-16"),
-    createData("3", "91011 Oak St", "2021-10-03", "John Smith", "2021-10-17"),
-    createData("4", "121314 Pine St", "2021-10-04", "Jane Smith", "2021-10-18"),
-    createData("5", "151617 Maple St", "2021-10-05", "John Johnson", "2021-10-19"),
-    createData("6", "181920 Birch St", "2021-10-06", "Jane Johnson", "2021-10-20"),
-    createData("7", "212223 Cedar St", "2021-10-07", "John Brown", "2021-10-21"),
-    createData("8", "242526 Walnut St", "2021-10-08", "Jane Brown", "2021-10-22"),
-    createData("9", "272829 Cherry St", "2021-10-09", "John White", "2021-10-23"),
-    createData("10", "303132 Peach St", "2021-10-10", "Jane White", "2021-10-24"),
-];
 */
