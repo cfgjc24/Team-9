9e6c21c6753a8ace37ab5c4e0598115b819be4b1
Casey Mak <casey.mak93@qmail.cuny.edu>
2024-09-20 21:34:53 -0400

    Added map routes, map logic, and directories

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import * as mapRoutes from './routes/mapRoutes.js';
+import mapRoutes from './routes/mapRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 5000; 
 const app = express();
 app.use(express.json());
 
+app.use('/api/map', mapRoutes);
+app.use('/api/user', userRoutes);
+
 app.listen(PORT, () => {
-    console.log(`App is listening on port : ${PORT}`);
+    console.log(`App is listening on port http://localhost:${PORT}`);
 })
