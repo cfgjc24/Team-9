4f9c8ff028399d1cd6d03dcc3f917bcb42282e8b
anjrajesh <anjrajesh16@gmail.com>
2024-09-21 07:26:59 -0400

    Made banner for styling

--- frontend/src/Pages/Dashboard/Dashboard.tsx
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {StickyHeadTable} from "../../Components/DataTable/DataTable";
+import "./Dashboard.css";
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <div><StickyHeadTable /></div>
+    <><div className="banner">
+      <h3><em>Lodestar is a light that guides the way.</em></h3>
+      <img src="https://cdn-icons-png.flaticon.com/512/3730/3730618.png" className="banner-image" alt="Lodestar icon" />
+    </div><div><StickyHeadTable /></div></>
   )
 }
 
